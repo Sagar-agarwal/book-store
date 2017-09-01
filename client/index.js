b3ddc82ce8b79da5ce162950c241dbbc3f8ab6ca
Sagar-agarwal <agarwal.sagar@outlook.com>
2017-09-01 18:27:00 +0530

    after babel run

---
+++ client/index.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+
+//React.createElement("h4", null, "Hello React 2"),
+<h2>Hello JSX</h2>,
+document.getElementById("root")
+);
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
+// );
